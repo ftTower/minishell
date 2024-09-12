d288bfaf12f2ad90b09ac7d4350225d3c0175865
zoyern <zoyern@gmail.com>
2024-09-12 04:18:04 +0200

    little advence in builtin debug pipe and execution corectly

--- src/pipex/soexec2.c
+++ src/pipex/soexec2.c
@@ -27,20 +27,17 @@ void	pipe_swap(int pipefd[2], int filefd[2])
 int	hub_builtin(t_mini *mini, char *cmd, int pipefd[2])
 {
 	if (!ft_strncmp("echo -n", cmd, 7))
-	{
-		write(1, "echo -n", 7);
-		write(pipefd[1], "echo -n", 7);
-		return (1);
-	}
+		return (putstrfd(cmd + 8, pipefd[1]), putstrfd("\n", pipefd[1]), 1);
 	if (!ft_strncmp("cd", cmd, 2))
 		return (is_raw_path(mini, cmd + 3), 1);
 	if (!ft_strncmp("pwd", cmd, 3))
-	{
-		putstrfd(get_envpl_var(mini, "PWD="), 1);
-		putstrfd(get_envpl_var(mini, "PWD="), pipefd[1]);
-
-		return (1);
-	}
+		return (putstrfd(get_envpl_var(mini, "PWD"), pipefd[1]), 1);
+	if (!ft_strncmp("export", cmd, 6))
+		return (putstrfd(get_envpl_var(mini, "PWD"), pipefd[1]), 1);
+	if (!ft_strncmp("unset", cmd, 5))
+		return (putstrfd(get_envpl_var(mini, "PWD"), pipefd[1]), 1);
+	if (!ft_strncmp("env", cmd, 3))
+		return (putstrfd(get_envpl_var(mini, "PWD"), pipefd[1]), 1);
 	return (0);
 }
 
@@ -49,7 +46,13 @@ int	exec_fork_child(t_mini *mini, int pipefd[2], int filefd[2], char *command)
 	int	ret;
 
 	if (hub_builtin(mini, command, pipefd))
-		return (0);
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+		close(filefd[0]);
+		close(filefd[1]);
+		return (mini->solib->close(mini->solib, EXIT_SUCCESS));
+	}
 	dup2(pipefd[0], 0);
 	dup2(pipefd[1], 1);
 	close(pipefd[0]);
@@ -57,7 +60,7 @@ int	exec_fork_child(t_mini *mini, int pipefd[2], int filefd[2], char *command)
 	close(filefd[0]);
 	close(filefd[1]);
 	ret = str_exec(mini, command);
-	mini->solib->close(mini->solib, EXIT_FAILURE);
+	mini->solib->close(mini->solib, ret);
 	return (ret);
 }
 
@@ -108,9 +111,15 @@ int	strs_exec(t_mini *mini, int fdin, char **commands, int fdout)
 	if (!commands || !*commands)
 		return (0);
 	soprintf("fdin : %d -- %d\n", fdin, fdout);
-	//file[read(fdin, file, 5000)] = '\0';
 	filefd[1] = fdout;
 	filefd[0] = fdin;
+	if (!commands[1])
+	{
+		if (hub_builtin(mini, *commands, filefd))
+			return (0);
+		if (!ft_strncmp("exit", *commands, 4))
+			return (close(fdin), close(fdout), mini->close(mini, EXIT_SUCCESS));
+	}
 	if (pipe(pipefd) == -1)
 		mini->solib->close(mini->solib, EXIT_FAILURE);
 	pipe_swap(pipefd, filefd);
